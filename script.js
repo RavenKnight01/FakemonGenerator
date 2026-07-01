8555489fbff22d3640d3e81be64fea8a396d80c9
Isis Arguello <89857164+RavenKnight01@users.noreply.github.com>
2026-06-30 22:05:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const base = [
-  "harvest mouse", 
+  "bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon", "charizard", "squirtle", "wartortle", "blastoise", "caterpie", "metapod", "butterfree", "weedle", "kakuna", 
 ];
 
 const ears = [
